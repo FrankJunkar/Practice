2d8898c8687043e985ddde58ed342db2191a2a0c
Frank Junkar <551617@my.cuhsd.org>
2020-10-15 18:58:04 +0000

    diggity doo

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import turtle as trtl
 trtl.pendown()
 trtl.speed(0)
 # jros uh surkul
-for i in range(720):
-  trtl.forward(0.5)
-  trtl.left(0.5)
+for i in range(18):
+  trtl.forward(2)
+  trtl.left(20)
+
+def diggityDoo():
+  print("diggity\ndaggity\ndig\ndag\ndoo")
+  print("yabadabadoo")
+  print("where are you")
+  print("hear me poo")
+
+diggityDoo();
